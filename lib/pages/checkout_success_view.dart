3568a51b921058bd4384c9c4c39be4c09e8649e1
Lini <119787571+B1nus@users.noreply.github.com>
2025-05-26 15:46:55 +0200

    Fixade se beställningar knappen

--- lib/pages/checkout_success_view.dart
+++ lib/pages/checkout_success_view.dart
@@ -2,6 +2,8 @@ import 'package:imat/app_theme.dart';
 import 'package:imat/widgets/app_navbar.dart';
 import 'package:flutter/material.dart';
 import 'package:imat/widgets/page_scaffold.dart';
+import 'package:imat/widgets/app_navbar.dart';
+import 'package:imat/pages/history_view.dart';
 
 
 class CheckoutSuccessView extends StatelessWidget {
@@ -64,11 +66,14 @@ class CheckoutSuccessView extends StatelessWidget {
         ElevatedButton(
           onPressed: () {
             // Navigate to history/orders view (you may need to adjust this)
-            Navigator.popUntil(context, (route) => route.isFirst);
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => HistoryView()),
+                        );
           },
           child: Text('Se beställningar'),
         ),
       ],
     );
-  }
+    }
 } 
